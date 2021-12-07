b5c95fbbb6b44206633f3c96b44e13fc445479fe
nepatil2 <nehapatil22899@gmail.com>
2021-12-07 12:20:13 +0530

    Solved Using Switch Case Statement

---
+++ EmployeeWageComputation4.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.problem;
+
+public class EmployeeWageComputation4 {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (empCheck){
+			case IS_FULL_TIME:
+				System.out.println("Employee is present for FULL TIME");
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee is present for PART TIME");
+				empHrs=4;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+				empHrs=0;
+		}
+
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Daily Wage = " + empWage);
+	}
+}
