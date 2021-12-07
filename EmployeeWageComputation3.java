68e0d7797e22378c65310eb734df088c20835324
nepatil2 <nehapatil22899@gmail.com>
2021-12-07 12:10:30 +0530

    Added Part Time Employee & Wage

--- EmployeeWageComputation3.java
+++ EmployeeWageComputation3.java
@@ -1,15 +1,20 @@
 package com.bridgelabz.problem;
 
-public class EmployeeWageComputation {
+public class EmployeeWageComputation3 {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is present for FULL TIME");
 			empHrs = 8;
+		}
+		else if (empCheck == IS_PART_TIME) {
+			System.out.println("Employee is present for PART TIME");
+			empHrs = 4;
 		}
 		else {
 			System.out.println("Employee is ABSENT");
