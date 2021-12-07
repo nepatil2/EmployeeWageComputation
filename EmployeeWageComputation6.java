9971ae34b1fab5c190ff4bd0d9a9f7d9cdae238d
nepatil2 <nehapatil22899@gmail.com>
2021-12-07 12:45:45 +0530

    Added Calculating Wages Till Month Reached

--- EmployeeWageComputation6.java
+++ EmployeeWageComputation6.java
@@ -1,17 +1,20 @@
 package com.bridgelabz.problem;
 
-public class EmployeeWageComputation5 {
+public class EmployeeWageComputation6 {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 
 	public static void main(String[] args) {
 		int empHrs = 0;
-		int empWage = 0;
-		int totalempWage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+			totalWorkingDays++ ;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 			switch (empCheck) {
 				case IS_FULL_TIME:
@@ -27,10 +30,10 @@ public class EmployeeWageComputation5 {
 					empHrs = 0;
 			}
 
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalempWage += empWage;
-			System.out.println("Emp Daily Wage = " + empWage);
+			totalEmpHrs += empHrs ;
+			System.out.println("Days:" +totalWorkingDays+  "  Emp Hr:"+empHrs);
 		}
-		System.out.println("Total Emp Wage = " + totalempWage);
+		int totalempWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage = "+totalempWage);
 	}
 }
